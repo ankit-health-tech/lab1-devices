42cba9dfe88a507f78244a11b7b1681be0eca28e
ankit-health-tech <50559812+ankit-health-tech@users.noreply.github.com>
2019-09-10 00:40:27 -0400

    Create blinkmyway.ino

---
+++ blinkmyway.ino
@@ -0,0 +1,60 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+ //My blinkmyway is a rudimentary renidition of eye of the tiger!
+ 
+ //pow
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(600);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(400);
+  
+  //pow pow pow
+  
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(500);
+
+  //pow pow pow
+  
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(500);
+
+  //pow pow poooooow
+  
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(2000);
+}
